54d9a0e3830a27731e74f45cae05e9a757e6054b
FRANCEBEAM <santanderfrancisbeam@gmail.com>
2022-12-29 22:45:24 +0800

    added hero sec and project sec

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,13 @@
 const openMenu = document.querySelector('.navbar-toggler');
 openMenu.addEventListener('click', () =>{
 
-    const navOpen = document.querySelector('.nav-li');
-    navOpen.classList.toggle("open")
-
+   //Menu Items
+   navOpen = document.querySelector('.nav-li')
+   navOpen.classList.toggle("open")
+   
+   //Burger Menu Animation
+   burgerMenu = document.querySelector('.navbar-toggler')
+   burgerMenu.classList.toggle("active")
+   console.log(1)
 });
 
