a3a0c4ea67dd50ad5afc3ed02592cf46bffe6e43
FRANCEBEAM <santanderfrancisbeam@gmail.com>
2022-12-29 17:51:04 +0800

    added burger menu

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+
+const openMenu = document.querySelector('.navbar-toggler');
+openMenu.addEventListener('click', () =>{
+
+    const navOpen = document.querySelector('.nav-li');
+    navOpen.classList.toggle("open")
+
+});
+
